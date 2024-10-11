c092d23891812a1c164bbc2a51d39460c7b7acb4
vishalmet <vishalmet003@gmail.com>
2024-10-11 15:26:51 +0530

    added sol token

--- src/components/Evaluate/Status.jsx
+++ src/components/Evaluate/Status.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Assets from '../Assets';
 
+const Status = ({ totalScore, tokenAge, honeypotStatus, owner }) => {
+  // Function to determine the background color based on the totalScore
+  const getBackgroundColor = (score) => {
+    if (score < 14.28) return '#6A116A'; // 0-14.28%
+    if (score < 28.56) return '#B40E00'; // 14.28-28.56%
+    if (score < 42.84) return '#DE9600'; // 28.56-42.84%
+    if (score < 57.12) return '#2CA3C0'; // 42.84-57.12%
+    if (score < 71.4) return '#469B44'; // 57.12-71.4%
+    if (score < 85.68) return '#37751D'; // 71.4-85.68%
+    return '#2D5D17'; // 85.68-100%
+  };
 
-
-const Status = ({ totalScore, tokenAge, honeypotStatus ,owner}) => {
   const handleCopy = () => {
     if (owner) {
       navigator.clipboard.writeText(owner).then(() => {
@@ -12,8 +21,8 @@ const Status = ({ totalScore, tokenAge, honeypotStatus ,owner}) => {
         console.error('Failed to copy: ', err);
       });
     }
-  
   }
+
   return (
     <div>
       <div className="flex gap-4 text-sm">
@@ -34,15 +43,17 @@ const Status = ({ totalScore, tokenAge, honeypotStatus ,owner}) => {
         <div className="space-y-3">
           <div className="w-fit">
             <p className='text-[#DDDDDD]'>Overall Score:</p>
-            <div className="bg-[#B40D00] h-10 w-full rounded-[8px] flex justify-center items-center mx-auto">
+            <div 
+              className="h-10 w-full rounded-[8px] flex justify-center items-center mx-auto" 
+              style={{ backgroundColor: getBackgroundColor(totalScore) }} // Set background color dynamically
+            >
               <p className='text-base font-semibold'>{totalScore}%</p>
             </div>
           </div>
           <div className="w-fit cursor-pointer" onClick={handleCopy}>
             <p className='text-[#DDDDDD] ' >Ownership:</p>
-            {/* {owner.slice(0,5)} */}
-            { owner !== '' && owner.slice(0,5)}    { owner !== '' && "..."}     { owner !== '' && owner.slice(-5)} 
-         {owner === '' &&    <img className='h-4 mx-auto' src={Assets.X} alt="X" />}
+            {owner !== '' && owner.slice(0, 5)} {owner !== '' && "..."} {owner !== '' && owner.slice(-5)} 
+            {owner === '' && <img className='h-4 mx-auto' src={Assets.X} alt="X" />}
           </div>
         </div>
       </div>
